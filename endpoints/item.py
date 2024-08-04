7e207e44f19c035785f2bffc63498f7fb0815ac7
Jarzt <dev.luanpham@gmail.com>
2024-08-04 20:32:35 +0700

    feat: init file

--- endpoints/item.py
+++ endpoints/item.py
@@ -1,11 +1,19 @@
+from utils.file_util import make_file_path, read_json
 from fastapi import APIRouter
 
-router = APIRouter()
-
-@router.get("/items/", tags=["users"])
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
-
-@router.get("/items/item_id", tags=["users"])
-async def read_user(item_id: int):
-    return {"item_id": item_id}
+class ItemEndpoint:
+    def __init__(self):
+        self.router = APIRouter()
+        self.tags = ["items"]
+        self.register_routes() 
+    
+    def register_routes(self):
+        @self.router.get("/items/")
+        def get_items(self):
+            item_data_path = make_file_path("item.json", "mocks")
+            item_data = read_json(item_data_path)
+            return item_data    
+            
+        @self.router.get("/items/{item_id}")
+        def get_item(self, item_id: int):
+            return {"item_id": item_id}
